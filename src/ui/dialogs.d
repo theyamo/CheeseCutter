c8b9d27449eeecac957fbbc17d3645461ede1665
Yamo the Great <ttaipalus@kapsi.fi>
2014-03-01 23:37:33 +0200

    year++

--- src/ui/dialogs.d
+++ src/ui/dialogs.d
@@ -194,7 +194,7 @@ class AboutDialog : Window {
 			screen.fprint(area.x + 1, y, "`01" ~ line.center(area.width-2));
 			y++;
 		}
-		screen.cprint(area.x + 1, y++,15, 0,"(C) 2009-13 Abaddon + contributors".center(area.width-2));
+		screen.cprint(area.x + 1, y++,15, 0,"(C) 2009-14 Abaddon + contributors".center(area.width-2));
 		screen.cprint(area.x + 1, y++,15, 0,"reSID engine by Dag Lem".center(area.width-2));
 		screen.cprint(area.x + 1, y++,15, 0,"Released under GNU GPL".center(area.width-2));
 		screen.fprint(area.x + 1, y++," ".center(area.width-2));
