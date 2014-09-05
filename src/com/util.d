e0fac03e10d6dd99847df3f85820df06a61e6c59
Yamo the Great <ttaipalus@kapsi.fi>
2014-09-05 07:55:45 +0300

    copied util.d from cc3

--- src/com/util.d
+++ src/com/util.d
@@ -61,6 +61,45 @@ string petscii2D(PetString petstring) {
 	return format(s);
 }
 
+string getArgumentValue(string argname, string[] text) {
+	foreach(line; text) {
+		string[] tokens = line.split();
+		if(tokens.length == 0) continue;
+		if(tokens[0] == argname && tokens.length > 2 && tokens[1] == "=")
+			return tokens[2];
+	}
+	return null;
+}
+
+ubyte[] table2Array(string table) {
+	static ubyte[4096] arr;
+	int idx;
+	writeln(table);
+	
+	foreach(strvalue; std.array.split(table)) {
+		munch(strvalue, "\r\n\t");
+		
+		arr[idx] = cast(ubyte)str2Value(strvalue);
+		writeln(arr[idx]);
+		idx++;
+	}
+	return arr[0..idx];
+}
+
+int str2Value(string s) {
+	if(s[0] == 'x' || s[0] == '$') {
+		int val, i;
+		foreach_reverse(c; toUpper(s[1..$])) {
+			if(c == 'x' || c == '$') break;
+			if("0123456789ABCDEF".indexOf(c) < 0)
+				throw new Error("Illegal hexadecimal value in string.");
+			val += ( (c >= '0' && c <= '9') ? c - '0' : c - ('A' - 10)) << (4 * i++);
+		}
+		return val;
+	}
+	return to!int(s);
+}
+
 int clamp(int n, int l, int h) { return n > h ? h : n < l ? l : n; }
 int umod(int n, int l, int h) { return n > h ? l : n < l ? h : n; }
 // 0-terminated string to d string
