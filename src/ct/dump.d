8f38d0e61882b176af4d3747daf645bbbc47e3df
theyamo <abad.flt@gmail.com>
2014-11-04 11:42:23 +0200

    replaced new dump.d

--- src/ct/dump.d
+++ src/ct/dump.d
@@ -2,6 +2,9 @@
 CheeseCutter v2 (C) Abaddon. Licensed under GNU GPL.
 */
 
+/** date to source dumper. very ugly quick and dirty hack
+ */
+
 module ct.dump;
 
 import ct.base;
@@ -9,122 +12,22 @@ import com.util;
 import std.string;
 
 private const string byteOp = "!byte", wordOp = "!word";
-private int highestChord, highestCmd, highestInstr,
-	highestPulse, highestFilter, highestWave;
 
-// ugly hack
-private int getHighestUsed(ubyte[] array) {
-	for(size_t i = array.length - 1; i >= 0; i--) {
-		if(array[i] > 0)
-			return cast(int)i;
-	}
-	return -1;
-}
-
-private void initTabLengths(Song sng) {
-	highestChord = highestCmd = highestInstr =
-		highestPulse = highestFilter = highestWave = 0;
-	
-	sng.seqIterator((Sequence seq, Element e) {
-			if(e.instr.hasValue() &&
-			   highestInstr < e.instr.value) {
-				highestInstr = e.instr.value;
-			}
-			if(e.cmd.value > 0) {
-				if(e.cmd.value < 0x40) {
-					if(highestCmd < (e.cmd.value & 0x1f)) {
-						highestCmd = e.cmd.value;
-					}
-				}
-				else if(e.cmd.value < 0x60) {
-					if(highestPulse < (e.cmd.value & 0x1f)) {
-						highestPulse = e.cmd.value & 0x1f;
-					}
-				}
-				else if(e.cmd.value < 0x80) {
-					if(highestFilter < (e.cmd.value & 0x1f)) {
-						highestFilter = e.cmd.value & 0x1f;
-					}
-				}
-				else if(e.cmd.value < 0xa0) {
-					if(highestChord < (e.cmd.value & 0x1f)) {
-						highestChord = e.cmd.value & 0x1f;
-					}
-				}
-			}
-		});
-
-	for(int i = 0; i < highestInstr; i++) {
-		int waveptr = sng.getWavetablePointer(i);
-		if(waveptr > highestWave) {
-			highestWave = waveptr;
-		}
-		int pulseptr = sng.getPulsetablePointer(i);
-		if(pulseptr > highestPulse) {
-			highestPulse = pulseptr;
-		}
-		int filtptr = sng.getFiltertablePointer(i);
-		if(filtptr > highestFilter) {
-			highestFilter = filtptr;
-		}
-	}
-
-	highestChord++;
-	highestCmd++;
-	highestInstr++;
-	highestPulse++;
-	highestFilter++;
-	
-	for(int i = 255; i >= highestWave; i--) {
-		if(sng.wave1Table[i] == 0x7e ||
-		   sng.wave1Table[i] == 0x7f) {
-			if(sng.wave1Table[i] == 0x7f && sng.wave2Table[i] > i)
-				throw new UserException("Overflow in wave table");
-			highestWave = i;
-			break;
-		}
-	}
-
-	// TO TEST: make last pulsetable entry point > current row
-	for(int i = 256 - 4; i >= 0; i -= 4) {
-		int pptr = sng.pulseTable[i + 3];
-		if(pptr < 0x40 && pptr > highestPulse) {
-			highestPulse = pptr;
-		}
-		if(pptr == 0x7f) {
-			highestPulse = i / 4;
-			break;
-		}
-	}
-	// TO TEST: make last filtertable entry point > current row
-	for(int i = 256 - 4; i >= 0; i -= 4) {
-		int fptr = sng.filterTable[i + 3];
-		if(fptr < 0x40 && fptr > highestPulse) {
-			highestFilter = fptr;
-		}
-		if(fptr == 0x7f) {
-			highestFilter = i / 4;
-			break;
-		}
-	}
-
-	// TODO: add code to test table overflows
-	
-	highestPulse++;
-	highestFilter++;
-	highestWave++;
-}
-
-		
 string dumpData(Song sng, string title) {
 	string output;
 
-	initTabLengths(sng);
-	
+	int getHighestUsed(ubyte[] array) {
+		for(size_t i = array.length - 1; i >= 0; i--) {
+			if(array[i] > 0)
+				return cast(int)i;
+		}
+		return -1;
+	}
+
 	void append(string s) {
 		output ~= s;
 	}
-
+	
 	void hexdump(ubyte[] buf, int rowlen) {
 		int c;
 		append("\t\t" ~ byteOp ~ " ");
@@ -140,52 +43,55 @@ string dumpData(Song sng, string title) {
 		}
 		append("\n");
 	}
-
-
 	int tablen;
 	ushort[][] trackpointers = new ushort[][sng.subtunes.numOf];
 	sng.subtunes.activate(0);
 	auto packedTracks = sng.subtunes.compact();	
 	
 	append( "arp1 = *\n");
-	hexdump(sng.wave1Table[0 .. highestWave], 16);
+	tablen = getHighestUsed(sng.wave1Table) + 1;
+	hexdump(sng.wave1Table[0 .. tablen], 16);
 	append( "arp2 = *\n");
-	hexdump(sng.wave2Table[0 .. highestWave], 16);
+	hexdump(sng.wave2Table[0 .. tablen], 16);
 	append( "filttab = *\n");
-	hexdump(sng.filterTable[0 .. highestFilter * 4], 4);
+	hexdump(sng.filterTable[0 .. getHighestUsed(sng.filterTable) + 4], 4);
 	append( "pulstab = *\n");
-	hexdump(sng.pulseTable[0 .. highestPulse * 4], 4);
+	hexdump(sng.pulseTable[0 .. getHighestUsed(sng.pulseTable) + 4], 4);
 	append( "inst = *\n");
 	ubyte[512] instab = 0;
 
+	int maxInsno;
+	sng.seqIterator((Sequence s, Element e) { 
+			int insval = e.instr.value;
+			if(insval > 0x2f) return;
+			if(insval > maxInsno) maxInsno = insval; });
 	for(int i = 0; i < 8; i++) {
 		append( format("\ninst%d = *\n",i));
-		hexdump(sng.instrumentTable[i * 48 .. i * 48 + highestInstr], 8);
+		hexdump(sng.instrumentTable[i * 48 .. i * 48 + (maxInsno+1)], 16);
 	}
 
+
 	append( "\nseqlo = *\n\t\t!8 ");
-	for(int i = 0; i < sng.numOfSeqs; i++) {
+	for(int i = 0; i < sng.numOfSeqs(); i++) {
 		append(format("<s%02x", i));
-		if(i < sng.numOfSeqs - 1)
+		if(i < sng.numOfSeqs() - 1)
 			append(",");
 	}
 	append( "\nseqhi = *\n\t\t!8 ");
-	for(int i = 0; i < sng.numOfSeqs; i++) {
+	for(int i = 0; i < sng.numOfSeqs(); i++) {
 		append(format(">s%02x", i));
-		if(i < sng.numOfSeqs - 1)
+		if(i < sng.numOfSeqs() - 1)
 			append(",");
 	}
 
-	//tablen = getHighestUsed(sng.superTable[0..64]) + 1;
-	//if(tablen < 1) tablen = 1;
-	if(highestCmd < 1) highestCmd = 1;
-	
+	tablen = getHighestUsed(sng.superTable[0..64]) + 1;
+	if(tablen < 1) tablen = 1;
 	append( "\ncmd1 = *\n");
-	hexdump(sng.superTable[0 .. highestCmd], 16);
+	hexdump(sng.superTable[0..tablen], 16);
 	append( "cmd2 = *\n");
-	hexdump(sng.superTable[64 .. 64 + highestCmd], 16);
+	hexdump(sng.superTable[64..64+tablen], 16);
 	append( "cmd3 = *\n");
-	hexdump(sng.superTable[128 .. 128 + highestCmd], 16);
+	hexdump(sng.superTable[128..128+tablen], 16);
 
 	// dump songsets
 
@@ -207,18 +113,23 @@ string dumpData(Song sng, string title) {
 		}
 	}
 
-	for(int i = 0; i < sng.numOfSeqs; i++) {
+	for(int i = 0; i < sng.numOfSeqs(); i++) {
 		append( format("s%02x = *\n", i));
 		Sequence s = sng.seqs[i];
 		hexdump(s.compact(), 16);
 	}
 
-	// TODO: rewrite to use indextable
 	tablen = getHighestUsed(sng.chordTable) + 1;
-	append("\nchord = *\n");
+	append("\nchord");
 	if(tablen < 1) tablen = 1;
-	hexdump(sng.chordTable[0 .. tablen], 16);
-	append("\nchordindex = *\n");
-	hexdump(sng.chordIndexTable[0 .. highestChord], 16);
+	hexdump(sng.chordTable[0..tablen], 16);
+	append("\nchordindex");
+	int highestChord = 0;
+	sng.seqIterator((Sequence s, Element e) { 				
+			if(e.cmd.value >= 0x80 && e.cmd.value <= 0x9f &&
+			   (e.cmd.value & 0x1f) > highestChord)
+				highestChord = e.cmd.value & 0x1f;
+		});
+	hexdump(sng.chordIndexTable[0..highestChord+1], 16);
 	return output;
 }
